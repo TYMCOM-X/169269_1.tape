333cf0ff27526d3ca8e6922a9a7e1812db6adf36
Lars Brinkhoff <lars@nocrew.org>
2021-03-12 06:09:00 +0000

    Files extracted from 169269_1.tape

---
+++ new11/lk522b.cmd
@@ -0,0 +1,9 @@
+sv522B/img/b:21000,mp522B=cg522B,ru522B,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,
+trm,trmf,trmp,trmt,zapbuf,
+LOAD,
+lnk1,lnk2,
+lnkb,lnkbs,lkbduv,crcbyt,lnkflg,
+lina,linb,auto13,once,end
+
