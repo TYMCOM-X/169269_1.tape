3898cdf97da03e4f0cd3391ff1d519bd1ef3c0e0
Lars Brinkhoff <lars@nocrew.org>
2021-03-12 03:07:05 +0000

    Files extracted from 169269_1.tape

---
+++ curr11/lk673.cmd
@@ -0,0 +1,6 @@
+SV673.IMG/img/b:1000,MP673.MAP=CG673,RU673,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,lnk1,lnk2,lnkf,LNKF1,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,TYMA,TYMB,
+lina,LINB,AUTO13,ONCE,END
