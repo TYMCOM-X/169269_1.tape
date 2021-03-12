413335b8945aff6e5144bdf79b67e0de5a387e95
Lars Brinkhoff <lars@nocrew.org>
2021-03-12 03:26:11 +0000

    Files extracted from 169269_1.tape

---
+++ curr11/lk371.cmd
@@ -0,0 +1,7 @@
+SV371.IMG/img/b:1000,MP371.MAP=CG371,RU371,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,TYMA,TYMB,
+lina,ONCE,END
