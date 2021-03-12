7486bbfc2445ab6a56f675eaf3fae95d0daef7b2
Lars Brinkhoff <lars@nocrew.org>
2021-03-12 06:05:03 +0000

    Files extracted from 169269_1.tape

---
+++ new11/lk556.cmd
@@ -0,0 +1,10 @@
+sv556/img/b:1000,mp556=cg556,ru556,XDT,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+LOAD,
+lnk1,lnk2,
+tyma,tymb,
+LNKG,LKGPKT,LKGQAL,CRCTAB,
+lina,once,end
+
